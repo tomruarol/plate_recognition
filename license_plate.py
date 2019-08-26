2ab4c02b3f9d0da7f4c15524d81eb1bb77374bd7
tomruarol <tomruarol@gmail.com>
2019-08-26 11:01:18 +0200

    Detect method

--- license_plate.py
+++ license_plate.py
@@ -30,3 +30,17 @@ class LicensePlateDetector:
         self.minPlateH = minPlateH # min height of the plate region
         self.numChars = numChars # number of characters to be detected in the plate, this is the number of characters our license plate has
         self.minCharW = minCharW # min width of the extracted characters, this is the min number of pixels wide a region must be to be considered a license plate character
+
+
+    def detect(self):
+		
+        lpRegions = self.detectPlates() # detect plate regions in the image
+ 
+        for lpRegion in lpRegions: # loop over the plate regions
+			
+            # detect character candidates in the current  plate region
+            lp = self.detectCharacterCandidates(lpRegion)
+ 
+            if lp.success: # only continue if characters were successfully detected
+
+                yield (lp, lpRegion) # yield a tuple of the plate object and bounding box: <palte, bouncing_box>
