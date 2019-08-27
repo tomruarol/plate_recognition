1c4839cd0aa4d5c2fb2f1053b7abcff1bbb34236
tomruarol <tomruarol@gmail.com>
2019-08-27 07:45:17 +0200

    Imports

---
+++ recognize.py
@@ -0,0 +1,8 @@
+# import the necessary packages
+from __future__ import print_function
+from plate_recognition.license_plate import LicensePlateDetector
+from imutils import paths
+import numpy as np
+import argparse
+import imutils
+import cv2
