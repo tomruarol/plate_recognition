8a4f56e6325356842b8aeb484588a88d59c1f248
tomruarol <tomruarol@gmail.com>
2019-08-27 07:45:49 +0200

    Argument parser

--- recognize.py
+++ recognize.py
@@ -6,3 +6,8 @@ import numpy as np
 import argparse
 import imutils
 import cv2
+
+# construct the argument parser and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--images", required=True, help="path to the images to be classified")
+args = vars(ap.parse_args())
